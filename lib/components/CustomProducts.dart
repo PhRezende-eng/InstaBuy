68c456d320565dcc22bbe06cf0f38f61cf91210d
PhRezende-eng <ph.hr.001@gmail.com>
2021-05-01 16:25:53 -0300

    #styleProject In this commit I create the Products components, I organized the code and I changed the name of some variables

---
+++ lib/components/CustomProducts.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+List<Widget> indexItems(List<dynamic> items) {
+  List<Widget> itemsWidget = [];
+
+  for (int i = 0; i < items.length; i++) {
+    itemsWidget.add(Container(
+      padding: EdgeInsets.only(top: 20),
+      alignment: Alignment.center,
+      width: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 5,
+              right: 5,
+            ),
+            child: Image.network(
+              'https://assets.instabuy.com.br/ib.item.image.small/s-${items[i]['images'][0]}',
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  '${items[i]['name']}',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                child: Text('R\$' +
+                    '${items[i]['prices'][0]['price'].toStringAsFixed(2)}'),
+              )
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+  return itemsWidget;
+}
--- lib/style.dart
+++ lib/style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color colorInstaB = Color(0xFFFF7E00);
+const Color orangeInstaB = Color(0xFFFF7E00);
+const Color greenInstaB = Color(0xff008004);
 const Color backGroundApp = Color(0xFFDBA5);
